174e49c5e5db051c775157569570a45cc1706c4f
obriencorey <cjobrien466@gmail.com>
2020-08-04 14:05:00 -0400

    updated august challenge palindrome

--- CPP_Challenges/AugustChallenge-Palindrome/aug-palindrome.cpp
+++ CPP_Challenges/AugustChallenge-Palindrome/aug-palindrome.cpp
@@ -9,32 +9,36 @@ public:
         int length = s.length();
         int counter = 0;
         int arraylength;
-        
-        char characters[length];
         bool output = true;
         
-        //loop through input string and if the character is alphanumeric, add it to char array
-        for(int i = 0; i < length; i++){
-            if((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123) || (s[i] > 47 && s[i] < 58)){
-                if(s[i] > 64 && s[i] < 91){
-                  characters[counter] = (s[i] + 32);
-                    counter++;  
-                } else{
-                characters[counter] = s[i];
-                counter++;
+        if(!s.empty()){
+            char characters[length];
+
+
+            //loop through input string and if the character is alphanumeric, add it to char array
+            for(int i = 0; i < length; i++){
+                if((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123) || (s[i] > 47 && s[i] < 58)){
+                    if(s[i] > 64 && s[i] < 91){
+                      characters[counter] = (s[i] + 32);
+                        counter++;  
+                    } else{
+                    characters[counter] = s[i];
+                    counter++;
+                    }
                 }
             }
-        }
-        //at this point, counter is now at the end of the array characters
-        
-        arraylength = counter;
-        counter--;
-        for(int i = 0; i < arraylength; i++){
-            cout << "Counting up: " << characters[i] << "      " << "Counting down: " << characters[counter] << endl;
-            if(characters[i] != characters[counter]){
-                output = false;
-            }
+            //at this point, counter is now at the end of the array characters
+
+            arraylength = counter;
             counter--;
+            for(int i = 0; i < arraylength; i++){
+                if(characters[i] != characters[counter]){
+                    output = false;
+                }
+                counter--;
+            }
+        } else {
+            output = true;
         }
         
         return(output);
@@ -43,11 +47,14 @@ public:
 
 int main(){
     Solution s;
-    std::string input = "RacECar";
+    std::string input;
+
+    cout << "Enter your string: " << endl;
+    cin >> input;
 
     if(s.isPalindrome(input)){
-        std::cout << "True" << std::endl;
+        cout << "Your string " << input <<  " is a palindrome" << endl;
     } else {
-        std::cout << "False" << std::endl;
+        cout << "Your string " << input <<  " is NOT a palindrome" << endl;
     }
 }
