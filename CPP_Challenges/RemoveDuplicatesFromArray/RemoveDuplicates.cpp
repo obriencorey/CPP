375978925b6709ae5708c46531718fbf8f215040
obriencorey <cjobrien466@gmail.com>
2020-08-07 14:23:07 -0400

    Remove duplicates added

---
+++ CPP_Challenges/RemoveDuplicatesFromArray/RemoveDuplicates.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int length = nums.size();
+        int previous;
+        int next;
+        int same = 0;   //increment if the same number is found
+        
+        if(!nums.empty()){
+            previous = nums[0];
+
+            for(int i = 1; i < length; i++){
+                next = nums[i];
+
+                if(previous == next){
+                    nums.erase(nums.begin() + i);
+                    i--;
+                }
+
+                previous = next;
+                length = nums.size();
+            }
+        }
+        
+        return(nums.size());
+    }
+};
+
+
+int main(){
+    vector<int> numbers;
+    int input;
+    int cinTerminator = -999;
+    Solution s;
+
+    cout << "Enter your numbers (enter -999 to exit): " << endl;
+    
+    while ((cin >> input) && input != cinTerminator) {
+        numbers.push_back(input);
+    }
+
+    s.removeDuplicates(numbers);
+
+    for(int i = 0; i < numbers.size(); i++){
+        cout << numbers[i] << " ";
+    }
+
+    cout << endl;
+ 
+}
